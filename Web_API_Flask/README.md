e2eac415fb67f3301f0b3f7d089eeadec8145095
shaunak1105 <shaunak1105@gmail.com>
2019-09-13 21:06:31 +0100

    db config + models

---
+++ Web_API_Flask/README.md
@@ -0,0 +1,20 @@
+### Run configurations
+
+We can create a new run configuration of type: `Flask Server` 
+in pycharm.
+
+While creating we can point the Target to our `app.py` file and
+set the `FLASK_DEBUG` environment variable to `True`.  This will 
+enable automatic refresh whenever the code is modified
+
+<img src="./img/diag1.png">
+
+### Install a new package on the venv
+
+To install a new package for the current venv:
+
+1. Go to File -> Settings -> Project -> Project Interpreter
+2. Here u can see all packages installed for 
+
+
+
--- Web_API_Flask/app.py
+++ Web_API_Flask/app.py
@@ -1,12 +1,83 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
 
 # this particular app takes its name from the name of the script
 app = Flask(__name__)
+# base dir: get path for the application
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+# app.config is a dict which holds all the configuration values.
+# But this is also where you can have your own configuration.
+# This is where we set the config for SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
 
+# now that config has been done, we can init our database
+db = SQLAlchemy(app)
+
+# DECORATOR: special capabilities to our functions
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/super_simple')
+def super_simple():
+    return jsonify(message="Hey there from the API!")
+
+
+# test endpoint that generates an error
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message="NOT FOUND"), 404
+
+# URL parameters
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough"), 401
+    else:
+        return jsonify(message="Welcome! " + name)
+
+
+# cleaner way to get URL parameters
+@app.route("/url_variables/<string:name>/<int:age>")
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough"), 401
+    else:
+        return jsonify(message="Welcome! " + name)
+
+
+# add classes for the db models
+class User(db.Model):
+    # set table name when table is created by SQLAlchemy
+    __tablename__ = 'users'
+    # set columns
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    # set table name when table is created by SQLAlchemy
+    __tablename__ = 'planets'
+
+    # set columns
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 
 if __name__ == '__main__':
