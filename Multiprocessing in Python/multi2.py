5f48a8dd3d315e72a8f2787a07242f8f3b5bab77
ShaunakSen <shaunak1105@gmail.com>
2020-06-12 21:57:41 +0530

    updated repo

--- Multiprocessing in Python/multi2.py
+++ Multiprocessing in Python/multi2.py
@@ -1,10 +1,11 @@
 from multiprocessing import Pool
 
 def job(num):
-    return num*2
+    return {'OK':[1,2,3]}
 
 if __name__ == '__main__':
     p = Pool(processes=20) # set up the Pool object, which will have 20 processes
-    data = p.map(job, [i for i in range(10000)])
+    data = p.map(job, [i for i in range(50)])
+    print (type(data))
     p.close()
     print (data)
