c00424139d30e5f85fb3f377eccafbe0e5dd0708
ShaunakSen <shaunak1105@gmail.com>
2020-06-12 23:35:00 +0530

    functions and images

--- Streamlit Apps for ML/app.py
+++ Streamlit Apps for ML/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
+
+def my_stupid_function(stupidity):
+    """
+    takes some stupidity and returns 1000x more stupidity
+    """
+    return 1000*stupidity
 
 st.title("Streamlit tutorial")
 
@@ -18,3 +26,20 @@ st.info("Info: test info")
 st.warning("Warning: test warning")
 st.error("Error: test error")
 st.exception(RuntimeError('This is an exception of type RuntimeError'))
+
+### Writing text: super functiom
+st.write(my_stupid_function(10))
+
+### print out documentation
+st.help(my_stupid_function)
+st.help(st.write)
+
+### displaying images and videos
+st.subheader("Displaying images and videos")
+st.markdown("---")
+img = Image.open('./carbon.png')
+st.image(img, caption="Simple image of code", use_column_width=True)
+
+### Working with widgets
+st.subheader("Working with widgets")
+st.markdown("---")
