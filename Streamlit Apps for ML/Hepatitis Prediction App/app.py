96d88d97d8649cdc3e575c376f6cda5f6aea6c56
ShaunakSen <shaunak1105@gmail.com>
2020-10-31 15:36:10 +0530

    setting up

---
+++ Streamlit Apps for ML/Hepatitis Prediction App/app.py
@@ -0,0 +1,24 @@
+### import core packages
+import streamlit as st
+import pandas as pd
+import os
+import joblib
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('agg')
+
+def main():
+    """
+    Hepatitis Mortality Prediction App
+    """
+    st.title("Disease Mortatlity Prediction App")
+    
+    menu = ['Home', 'Login', 'Register']
+    submenu = ['Visualizations', 'Predictions']
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
