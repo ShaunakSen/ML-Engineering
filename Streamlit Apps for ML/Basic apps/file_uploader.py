931c124ff335a13b6746663ee162b74f5eff53fb
ShaunakSen <shaunak1105@gmail.com>
2020-11-18 20:17:12 +0530

    working with images

--- Streamlit Apps for ML/Basic apps/file_uploader.py
+++ Streamlit Apps for ML/Basic apps/file_uploader.py
@@ -6,6 +6,16 @@ import pandas as pd
 import numpy as np
 import pickle
 
+
+### load image
+
+from PIL import Image
+
+@st.cache
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
+
 def main():
     st.title("File upload")
 
@@ -15,6 +25,22 @@ def main():
 
     if choice == menu[0]:
         st.subheader(menu[0])
+
+        ### Image 
+        image_file = st.file_uploader(label='Upload Image', type=['png', 'jpg', 'jpeg'])
+        ### some info about the file
+        if image_file:
+            ## see the details
+            st.write(type(image_file))
+            st.write(dir(image_file))
+            file_details = {
+                "filename": image_file.name,
+                "filetype": image_file.type,
+                "filesize": image_file.size
+            }
+            st.write(file_details)
+
+            st.image(image=load_image(image_file), caption=file_details['filename'], width=500, height=500)
 
     elif choice == menu[1]:
         st.subheader(menu[1])
